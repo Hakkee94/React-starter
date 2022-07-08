2c265bc0c28946708c6d410eff3c34f74e4bf9c1
Greyewi <greyewi@gmail.com>
2022-07-08 18:00:30 +0500

    add react router

--- mini-projects/mini-projects/src/App.js
+++ mini-projects/mini-projects/src/App.js
@@ -2,25 +2,35 @@ import './App.css';
 import WorldClock from "./Components/WorldClock/WorldClock";
 import AlarmApp from "./Components/AlarmApp/Components/AlarmApp";
 import Chat from "./Components/AnonymousChat";
+import PhotoManager from './Components/PhotoManager/PhotoManager'
 import Crud from "./Components/Crud/Crud";
-
-window.data = {id: 55, city: {zip: 333333, name: "Moscow"}}
+import {
+  Routes,
+  Route,
+  NavLink
+} from "react-router-dom";
 
 function App() {
-  const [state, setState] = useState(true)
-
+    const activeStyle = {backgroundColor: "green"}
 
     return (
         <div className="App">
-        <input onChange={e => {
-          window.data = {id: 55, city: {zip: e.target.value, name: "Moscow"}}
-          console.log("app data", window.data)
-        }}/>
-            {/*<PhotoManager/>*/}
-            {/*<WorldClock/>*/}
-            {/*<AlarmApp/>*/}
-            {/*<Chat/>*/}
-            <Crud/>
+          <NavLink to="/some" style={({ isActive }) => isActive ? {backgroundColor: "green"} : undefined }>Some</NavLink>
+          <NavLink to="/photo-manager" style={({ isActive }) => isActive ? {backgroundColor: "green"} : undefined } >PhotoManager</NavLink>
+          <NavLink to="/clock" style={({ isActive }) => isActive ? activeStyle : undefined } >WorldClock</NavLink>
+          <NavLink to="/alarm" style={({ isActive }) => isActive ? activeStyle : undefined } >AlarmApp</NavLink>
+          <NavLink to="/chat" style={({ isActive }) => isActive ? activeStyle : undefined }>Chat</NavLink>
+          <NavLink to="/crud" style={({ isActive }) => isActive ? activeStyle : undefined }>Crud</NavLink>
+
+          <Routes>
+            <Route path="/some" element={<>List</>}/>
+
+            <Route path="/photo-manager" element={<PhotoManager/>}/>
+            <Route path="/clock" element={<WorldClock/>}/>
+            <Route path="/alarm" element={<AlarmApp/>}/>
+            <Route path="/chat" element={<Chat/>}/>
+            <Route path="/crud" element={<Crud/>}/>
+          </Routes>
         </div>
     );
 }
