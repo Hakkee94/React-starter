68a77742a3700310706b07b20044cf185a933a65
Greyewi <greyewi@gmail.com>
2022-07-08 17:25:27 +0500

    some fixes

--- mini-projects/mini-projects/src/App.js
+++ mini-projects/mini-projects/src/App.js
@@ -1,13 +1,23 @@
 import './App.css';
 import WorldClock from './Components/WorldClock/Index'
+import {useState} from "react";
+import { v4 as uuidv4 } from 'uuid';
+
+window.data = {id: 55, city: {zip: 333333, name: "Moscow"}}
 
 function App() {
-
+  const [state, setState] = useState(true)
 
     return (
         <div className="App">
+        <input onChange={e => {
+          window.data = {id: 55, city: {zip: e.target.value, name: "Moscow"}}
+          console.log("app data", window.data)
+        }}/>
             {/*<PhotoManager/>*/}
-            <WorldClock/>
+          <button onClick={() => setState(!state)}>Click</button>
+          {state && <WorldClock location={window.data}/>}
+          {uuidv4()}
         </div>
     );
 }
